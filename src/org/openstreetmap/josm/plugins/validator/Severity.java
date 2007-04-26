175b69bdf35dea9c3805eab7569d6069fbd4032d
Francisco R. Santos <frsantos@gmail.com>
2007-04-26 20:20:17 +0000

    Added a new layer where all errors are highlighted

---
+++ src/org/openstreetmap/josm/plugins/validator/Severity.java
@@ -0,0 +1,63 @@
+package org.openstreetmap.josm.plugins.validator;
+
+import java.awt.Color;
+
+import org.openstreetmap.josm.data.osm.visitor.SimplePaintVisitor;
+
+/** The error severity */
+public enum Severity {
+    /** Error messages */
+    ERROR("Errors", "error.gif",       SimplePaintVisitor.getPreferencesColor("validation error", Color.RED)),
+    /** Warning messages */ 
+    WARNING("Warnings", "warning.gif", SimplePaintVisitor.getPreferencesColor("validation warning", Color.YELLOW)), 
+    /** Other messages */ 
+    OTHER("Other", "other.gif",        SimplePaintVisitor.getPreferencesColor("validation other", Color.CYAN)); 
+    
+    /** Description of the severity code */
+    private final String message;
+    
+    /** Associated icon */
+    private final String icon;
+
+    /** Associated color */
+    private final Color color;
+
+    /**
+     * Constructor
+     * 
+     * @param message Description
+     * @param icon Associated icon
+     */
+    Severity(String message, String icon, Color color) 
+    {
+        this.message = message;
+        this.icon = icon;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return message;
+    }
+
+    /** 
+     * Gets the associated icon
+     * @return the associated icon
+     */
+    public String getIcon() 
+    {
+        return icon;
+    }
+
+    /**
+     * Gets the associated color
+     * @return The associated color
+     */
+    public Color getColor()
+    {
+        return color;
+    }
+    
+    
+}
